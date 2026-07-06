23b2013e4c8f609980da4777b951e5981c31d539
Adrian <264207672+blond3334d@users.noreply.github.com>
2026-07-07 00:42:30 +0800

    Add a function that takes two numbers and an operator.

--- codes/main.js
+++ codes/main.js
@@ -1,13 +1,81 @@
-const userInput = document.getElementById("userInput");
 
+// Input
+const result = document.getElementById("userInput");
+const numberBtn = document.querySelectorAll(".numberBtn");
+const operator = document.querySelectorAll(".operator");
+
+let firstInput = "";
+let currentOperator = "";
+let secondInput = "";
+
+operator.forEach((element) => {
+    element.addEventListener("click", e => {
+        currentOperator = e.target.textContent;
+    })
+});
+
+numberBtn.forEach((number) => {
+    number.addEventListener("click", btn => {
+        if (currentOperator === "") {
+            firstInput += btn.target.textContent;
+            if (btn.target.textContent === '.' && firstInput.includes('.')) {
+                document.querySelector('.dot').disabled = true;
+            }
+            console.log(`First ${firstInput}`);
+        } else {
+            document.querySelector('.dot').disabled = false;
+            secondInput += btn.target.textContent;
+            if (btn.target.textContent === '.' && secondInput.includes('.')) {
+                document.querySelector('.dot').disabled = true;
+            }
+            console.log(secondInput);
+        }
+
+        
+    })
+});
+
+// Operations
+function add(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+function subtract(firstNum, secondNum) {
+    return firstNum - secondNum;
+}
+function multiply(firstNum, secondNum) {
+    return firstNum * secondNum;
+}
+function divide(firstNum, secondNum) {
+    return firstNum / secondNum;
+}
+
+// Calculate
+function operate() {
+    switch(currentOperator) {
+        case '&plus':
+            result.value = add(firstInput, secondInput);
+            break;
+        case '&minus':
+            result.value = subtract(firstInput, secondInput);
+            break;
+        case '&times':
+            result.value = multiply(firstInput, secondInput);
+            break;
+        case '&divide':
+            result.value = divide(firstInput, secondInput);
+            break;
+    }
+}
+
+// Displaying
 function appendToDisplay(input) {
-    userInput.value += input;
+    result.value += input;
 }
 
 function clearDisplay(){
-    userInput.value = "";
+    result.value = "";
 }
 
 function calculateResult() {
-    userInput.value = eval(userInput.value);
+    result.value = operate();
 }
