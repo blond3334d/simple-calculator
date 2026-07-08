0b735976bf2d19ce2500aff17414e72d1a85a683
Adrian <264207672+blond3334d@users.noreply.github.com>
2026-07-08 23:20:00 +0800

    Chore: Allow multiple operations and add keyboard functionality

--- codes/main.js
+++ codes/main.js
@@ -1,7 +1,16 @@
 let currentDisplay = document.getElementById("userInput");
 const numberBtns = document.querySelectorAll(".numberBtn");
 const operators = document.querySelectorAll(".operator");
+let equalBtn = document.querySelector('.equalBtn');
 let hasDot = document.querySelector('.dot');
+
+let isClicked = false;
+let result = 0;
+
+equalBtn.addEventListener("click", e => {
+    operate();
+    isClicked = true;
+});
 
 const operations = {
     '+': (firstNum, secondNum) => firstNum + secondNum,
@@ -18,16 +27,24 @@ const expression = {
     waitForSecondOperand: false,
 };
 
+const temp = {
+    tempValue: '0',
+    tempFirstOperand: null,
+    tempCurrentOperator: null,
+    tempSecondOperand: null,
+};
+
 // Getting the operator
 operators.forEach(op => {
     op.addEventListener("click", e => {
         expression.currentOperator = e.target.textContent;
         expression.waitForSecondOperand = true;
         currentDisplay.value = "";
-        hasDot.disabled = false;
         expression.displayValue = "";
+        hasDot.disabled = false;
 
         console.log(expression.currentOperator);
+
     })
 });
 
@@ -52,17 +69,51 @@ numberBtns.forEach(num => {
 
             console.log(`First ${expression.firstOperand}`);
         }
+
     });
 });
+
+
+
+// if (expression.secondOperand !== '0' && isClicked === false) {
+//     operate();
+//     for (key in expression) {
+//         expression[key] = '0';
+//     }
+//     expression.currentOperator = op.textContent;
+//     expression.firstOperand = result;
+// }
+
+
+
+
+
+// if (isClicked != true) {
+//     for (key in expression) {
+//         if (expression[key] !== '0') {
+//             temp.tempFirstOperand = expression.firstOperand;
+//             temp.tempCurrentOperator = expression.currentOperator;
+//             temp.secondOperand = expression.secondOperand;
+//         }
+//         expression[key] = '0';
+//     }
+
+//     operate();
+//     expression.firstOperand = currentDisplay.value;
+// }
 
 // Calculate result
 function operate() {
     let a = expression.firstOperand;
     let b = expression.secondOperand;
     let op = expression.currentOperator;
+    // let a = temp.tempFirstOperand;
+    // let b = temp.tempSecondOperand;
+    // let op = temp.tempCurrentOperator;
 
     if (operations[op]) {
-        currentDisplay.value = operations[op](a, b);
+        result = operations[op](a, b);
+        currentDisplay.value = result;
     }
 
     return null;
