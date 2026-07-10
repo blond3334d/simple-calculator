e64e1b2a9e41ddfd8c3909dc52229fedeb7e5c7d
Adrian <264207672+blond3334d@users.noreply.github.com>
2026-07-10 23:26:27 +0800

    Rewrite logic. Still need some fixing

--- codes/main.js
+++ codes/main.js
@@ -1,145 +1,152 @@
-let currentDisplay = document.getElementById("userInput");
-const numberBtns = document.querySelectorAll(".numberBtn");
-const operators = document.querySelectorAll(".operator");
-let equalBtn = document.querySelector('.equalBtn');
-let hasDot = document.querySelector('.dot');
+const numberBtns = document.querySelectorAll('.numberBtn');
+const operatorBtns = document.querySelectorAll('.operatorBtn');
+const equalBtn = document.querySelector('.equalBtn');
+const allClearBtn = document.querySelector('.all-clear');
+const deleteBtn = document.querySelector('.delete');
+const display = document.querySelector('.displaySpace');
 
-let clearIsClicked = false;
-let equalIsClicked = false;
-let opIsClicked = false;
-let on = false;
-let newOperator = null;
-let result = 0;
+// const operations = {
+//     '+': (firstNum, secondNum) => firstNum + secondNum,
+//     '−': (firstNum, secondNum) => firstNum - secondNum,
+//     '×': (firstNum, secondNum) => firstNum * secondNum,
+//     '÷': (firstNum, secondNum) => firstNum / secondNum,
+// };
 
-const operations = {
-    '+': (firstNum, secondNum) => firstNum + secondNum,
-    '−': (firstNum, secondNum) => firstNum - secondNum,
-    '×': (firstNum, secondNum) => firstNum * secondNum,
-    '÷': (firstNum, secondNum) => firstNum / secondNum,
-};
+function add(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+function subtract(firstNum, secondNum) {
+    return firstNum - secondNum;
+}
+function multiply(firstNum, secondNum) {
+    return firstNum * secondNum;
+}
+function divide(firstNum, secondNum) {
+    return firstNum / secondNum;
+}
 
-const expression = {
-    displayValue: '0',
-    firstOperand: null,
-    currentOperator: null,
-    secondOperand: null,
-    waitForSecondOperand: false,
-};
+// Factory function
+let expression = mathExpresion();
+function mathExpresion() {
+    return {
+        displayValue: '',
+        firstOperand: '',
+        currentOperator: undefined,
+        secondOperand: '',
+        waitForSecondOperand: false,
+    };
+}
 
 // Calculate result
 function operate() {
-    let a = expression.firstOperand;
-    let b = expression.secondOperand;
+    let a = parseFloat(expression.secondOperand);
+    let b = parseFloat(expression.firstOperand); 
     let op = expression.currentOperator;
 
-    if (operations[op]) {
-        result = operations[op](a, b);
-        return currentDisplay.value = result;
+    // if (operations[op]) {
+    //     result = operations[op](a, b);
+    //     return display.value = result;
+    // } else {
+    //     return null;
+    // }
+
+    let result
+
+    if (isNaN(a) || isNaN(b)) {
+        return;
     } else {
-        return null;
-    }
-}
-
-// Clear display
-function clearDisplay() {
-    currentDisplay.value = "";
-    clearExpressionValue();
-
-    clearIsClicked = true;
-    console.log(Object.values(expression));
-}
-
-// Clear key values
-function clearExpressionValue() {
-    for (key in expression) {
-        expression[key] = '0';
-    }
-
-    return expression;
-}
-
-
-
-
-// Getting the operator
-operators.forEach(op => {
-    op.addEventListener("click", e => {
-        if (
-            expression.firstOperand !== null &&
-            expression.currentOperator !== null &&
-            expression.secondOperand !== null
-        ) {
-            operate();
-
-            // for (key in expression) {
-            //     expression[key] = '0';
-            // }
-            expression.currentOperator = null;
-            expression.secondOperand = null;
-            expression.waitForSecondOperand = false;
-            expression.displayValue = '0';
-
-            if (clearIsClicked !== true) {
-                expression.firstOperand = result;
-            }
-
-            newOperator = e.target.textContent; 
-            expression.currentOperator = newOperator;
-
-            console.log(Object.values(expression));
-
-            // return currentDisplay.value = result;
-        } else {
-            expression.currentOperator = e.target.textContent;
-            expression.waitForSecondOperand = true;
-            expression.displayValue = "";
-            hasDot.disabled = false;
-            on = true;
+        switch(op) {
+            case '+':
+                result = add(a, b);
+                break;
+            case '−':
+                result = subtract(a, b);
+                break;
+            case '×':
+                result = multiply(a, b);
+                break;
+            case '÷':
+                result = divide(a, b);
+                break;
+            default:
+                return;
         }
+    }
 
-        // console.log(expression.currentOperator);
+    return display.value = result;
+}
+
+// Select/display the number
+function selectNumber(number) {
+    if (number === '.' && expression.firstOperand.includes('.')) return;
+    expression.firstOperand = expression.firstOperand.toString() + number.toString();
+
+    // if (expression.currentOperator !== undefined) {
+    //     expression.secondOperand = expression.secondOperand.toString() + number.toString();
+    //     console.log(`Second ${expression.secondOperand}`);
+    // }
+}
+
+// Update the display
+function updateDisplay() {
+    display.value = expression.firstOperand;
+
+    if (expression.firstOperand !== '') {
+        
+        display.value = expression.firstOperand;
+    }
+}
+
+// Clear all
+function clearAll() {
+    expression = mathExpresion();
+    // display.value = "";
+}
+
+// Delete a number
+function deleteNum() {
+    expression.firstOperand = expression.firstOperand.toString().slice(0, -1);
+}
+
+// Select operator
+function selectOperator(operation) {
+    if (expression.firstOperand === '') return
+    if (expression.secondOperand !== '') {
+        operate();
+    }
+
+    expression.currentOperator = operation;
+    expression.secondOperand = expression.firstOperand;
+    expression.firstOperand = '';
+}
+
+
+// Get user's input
+numberBtns.forEach(btn => {
+    btn.addEventListener("click", e => {
+        selectNumber(e.target.innerText);
+        updateDisplay();
     })
-});
+})
 
-// Getting the numbers
-numberBtns.forEach(num => {
-    num.addEventListener("click", e => {
-        if (expression.waitForSecondOperand === true) {
-            if (e.target.textContent.includes('.')) {
-                hasDot.disabled = true;
-            }
+operatorBtns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        selectOperator(e.target.innerText);
+        updateDisplay();
+    })
+})
 
-            if (on === true) {
-                currentDisplay.value = "";
-                on = false;
-            }
+allClearBtn.addEventListener("click", clear => {
+    clearAll();
+    updateDisplay();
+})
 
-            expression.displayValue += e.target.textContent;
-            currentDisplay.value += e.target.textContent;
-            expression.secondOperand = parseFloat(expression.displayValue);
-            console.log(`Second ${expression.secondOperand}`);
-        } else {
-            if (e.target.textContent.includes('.')) {
-                hasDot.disabled = true;
-            }
-            expression.displayValue += e.target.textContent;
-            currentDisplay.value += e.target.textContent;
-            expression.firstOperand = parseFloat(expression.displayValue);
-
-            console.log(`First ${expression.firstOperand}`);
-        }
-
-    });
-});
-
-equalBtn.addEventListener("click", e => {
+equalBtn.addEventListener("click", () => {
     operate();
-    clearExpressionValue();
-    console.log(Object.values(expression));
+    updateDisplay();
+})
 
-    equalIsClicked = true;
-});
-
-
-
-
+deleteBtn.addEventListener("click", () => {
+    deleteNum();
+    updateDisplay();
+})
