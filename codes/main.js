9e0d29a79fc4d6637fccdcd8ae267cb7c318af98
Adrian <264207672+blond3334d@users.noreply.github.com>
2026-07-11 19:03:34 +0800

    Do some restyling. Add all the functionalities. Chore: keyboard support.

--- codes/main.js
+++ codes/main.js
@@ -5,102 +5,57 @@ const allClearBtn = document.querySelector('.all-clear');
 const deleteBtn = document.querySelector('.delete');
 const display = document.querySelector('.displaySpace');
 
-// const operations = {
-//     '+': (firstNum, secondNum) => firstNum + secondNum,
-//     '−': (firstNum, secondNum) => firstNum - secondNum,
-//     '×': (firstNum, secondNum) => firstNum * secondNum,
-//     '÷': (firstNum, secondNum) => firstNum / secondNum,
-// };
-
-function add(firstNum, secondNum) {
-    return firstNum + secondNum;
-}
-function subtract(firstNum, secondNum) {
-    return firstNum - secondNum;
-}
-function multiply(firstNum, secondNum) {
-    return firstNum * secondNum;
-}
-function divide(firstNum, secondNum) {
-    return firstNum / secondNum;
-}
+const operations = {
+    '+': (firstNum, secondNum) => firstNum + secondNum,
+    '−': (firstNum, secondNum) => firstNum - secondNum,
+    '×': (firstNum, secondNum) => firstNum * secondNum,
+    '÷': (firstNum, secondNum) => firstNum / secondNum,
+};
 
 // Factory function
 let expression = mathExpresion();
 function mathExpresion() {
     return {
-        displayValue: '',
         firstOperand: '',
-        currentOperator: undefined,
+        currentOperator: '',
         secondOperand: '',
-        waitForSecondOperand: false,
     };
 }
 
 // Calculate result
 function operate() {
+    let result
     let a = parseFloat(expression.secondOperand);
     let b = parseFloat(expression.firstOperand); 
     let op = expression.currentOperator;
 
-    // if (operations[op]) {
-    //     result = operations[op](a, b);
-    //     return display.value = result;
-    // } else {
-    //     return null;
-    // }
-
-    let result
-
-    if (isNaN(a) || isNaN(b)) {
-        return;
+    if (operations[op]) {
+        result = operations[op](a, b);
+        if (b === 0) return display.value = 'ERROR';
+        return display.value = +result.toFixed(7);
     } else {
-        switch(op) {
-            case '+':
-                result = add(a, b);
-                break;
-            case '−':
-                result = subtract(a, b);
-                break;
-            case '×':
-                result = multiply(a, b);
-                break;
-            case '÷':
-                result = divide(a, b);
-                break;
-            default:
-                return;
-        }
+        return;
     }
-
-    return display.value = result;
 }
 
 // Select/display the number
 function selectNumber(number) {
+    if (display.value == operate()) {
+        clearAll();
+    }
+
     if (number === '.' && expression.firstOperand.includes('.')) return;
     expression.firstOperand = expression.firstOperand.toString() + number.toString();
-
-    // if (expression.currentOperator !== undefined) {
-    //     expression.secondOperand = expression.secondOperand.toString() + number.toString();
-    //     console.log(`Second ${expression.secondOperand}`);
-    // }
 }
 
 // Update the display
 function updateDisplay() {
     display.value = expression.firstOperand;
-
-    if (expression.firstOperand !== '') {
-        
-        display.value = expression.firstOperand;
-    }
 }
 
 // Clear all
 function clearAll() {
     expression = mathExpresion();
-    // display.value = "";
 }
 
 // Delete a number
@@ -112,7 +67,8 @@ function deleteNum() {
 function selectOperator(operation) {
     if (expression.firstOperand === '') return
     if (expression.secondOperand !== '') {
-        operate();
+        display.value = operate();
+        expression.firstOperand = operate();
     }
 
     expression.currentOperator = operation;
@@ -131,8 +87,12 @@ numberBtns.forEach(btn => {
 
 operatorBtns.forEach(btn => {
     btn.addEventListener('click', e => {
-        selectOperator(e.target.innerText);
         updateDisplay();
+        selectOperator(e.target.innerText);
+
+        if(e.target.innerText !== '' && expression.secondOperand !== '') {
+            display.value = expression.secondOperand;
+        }
     })
 })
 
@@ -142,11 +102,14 @@ allClearBtn.addEventListener("click", clear => {
 })
 
 equalBtn.addEventListener("click", () => {
-    operate();
     updateDisplay();
+    operate();
 })
 
 deleteBtn.addEventListener("click", () => {
+    if (display.value == operate()) {
+       clearAll();
+    }
     deleteNum();
     updateDisplay();
 })
